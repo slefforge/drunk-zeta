8a033a121753104aa9a77ad6376dac5748e44c98
Sam <sam@starke.gg>
2023-02-10 10:21:23 -0600

    Fix config.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import asyncio
 import discord
+import random
 from discord import app_commands
 from discord.ext import commands
 from discord.app_commands.commands import Command
 from config import TOKEN
+from messages import *
 
-bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
+bot = commands.Bot(command_prefix='', intents = discord.Intents.all())
 shotsTotal = 0
 
 @bot.event
@@ -20,56 +23,64 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def hey(ctx):
+    await ctx.send(random.choice(heyList))
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(random.choice(heyList))
+
+@bot.command()
+async def yo(ctx):
+    await ctx.send(random.choice(heyList))
+
+@bot.command()
+async def hi(ctx):
+    await ctx.send(random.choice(heyList))
+
+@bot.command()
+async def bitch(ctx):
+    await ctx.send("wow")
+
+
+
 @bot.tree.command(name='giveshot')
 @app_commands.describe(type = "What type of alcohol?", name = "What is the name of the shot?")
 async def shot(interaction: discord.Interaction, type: str, name: str):
     global shotsTotal
-    shotEffects = [ #25
-        'Drunk Zeta is feeling completely sober.',
-        'Drunk Zeta is slightly buzzed.',
-        'Drunk Zeta is feeling a little tipsy.',
-        'Drunk Zeta has a feeling of relaxation and sociability.',
-        'Drunk Zeta has increased confidence and talkativeness.',
-        'Drunk Zeta has mild impairments in judgement and coordination.',
-        'Drunk Zeta has increased likelihood of engaging in risky behavior.',
-        'Drunk Zeta has mild to moderate impairments in memory and cognitive function.',
-        'Drunk Zeta has slurred speech and difficulty walking.',
-        'Drunk Zeta is starting to feel quite nauseous.',
-        'Drunk Zeta has impaired balance and coordination.',
-        'Drunk Zeta has extreme mood swings and emotional outbursts.',
-        'Drunk Zeta now has blackouts and memory lapses.',
-        'Drunk Zeta now has a severe loss of coordination and balance.',
-        'Drunk Zeta now has possible alcohol poisoning.',
-        'Drunk Zeta definitely now has alcohol poisoning.',
-        'Drunk Zeta is now passed out on the floor.',
-        'I\'m not sure what happened to Drunk Zeta, but I think he died.',
-        'Drunk Zeta is now pronounced dead.',
-        'Drunk Zeta is now a zombie.',
-        'Drunk Zeta still manages to drink alcohol. Despite being a zombie.',
-        'Drunk Zeta is now a black hole of alcohol.',
-        'The Universe is now collapsing due to Drunk Zeta\'s alcohol consumption.',
-        'The Universe has collapsed due to Drunk Zeta\'s alcohol consumption.',
-        'You are no longer able to give Drunk Zeta any more shots, as there is no more Universe left.',
-    ]
     if shotsTotal < 25:
         shotsTotal += 1
         embed = discord.Embed(description=f'Drunk Zeta has been given a `{type}` shot called `{name}` from {interaction.user.mention}. 🍻')
-        embed.add_field(name="Drunk Zeta's shot total:", value=f'{shotsTotal} shot(s). {shotEffects[shotsTotal-1]}')
+        embed.add_field(name="Drunk Zeta's shot total:", value=f'`{shotsTotal}` shot(s). {shotEffects[shotsTotal-1]}')
     else:
         embed = discord.Embed(description=f'{interaction.user.mention}, you have given Drunk Zeta too many shots. 🍻')
-        embed.add_field(name="Drunk Zeta's shot total:", value=f'{shotsTotal} shot(s). {shotEffects[24]}')
+        embed.add_field(name="Drunk Zeta's shot total:", value=f'`{shotsTotal}` shot(s). {shotEffects[24]}')
     await interaction.response.send_message(embed=embed)
 
 @bot.tree.command(name='totalshots')
 async def totalshots(interaction: discord.Interaction):
-    await interaction.response.send_message(f'Drunk Zeta\'s shot total is {shotsTotal} shots.')
+    embed = discord.Embed(title=f'Drunk Zeta\'s shot total is `{shotsTotal}` shots.', description=f'{shotEffects[shotsTotal-1]}')
+    await interaction.response.send_message(embed=embed)
 
+@bot.tree.command(name='exist')
+async def exist(interaction: discord.Interaction):
+    await interaction.response.send_message(embed=discord.Embed(title=random.choice(existences), color=random.randint(0, 0xFFFFFF)))
+
+@bot.tree.command(name='help')
+async def help(interaction: discord.Interaction):
+    embed = discord.Embed(title='Drunk Zeta\'s help menu', description='Drunk Zeta is a bot that needs your help getting drunk.')
+    embed.add_field(name='/giveshot', value='Give Drunk Zeta a shot. 🍻', inline=False)
+    embed.add_field(name='/totalshots', value='See how many shots Drunk Zeta has had.', inline=False)
+    embed.add_field(name='/exist', value='Ask Drunk Zeta to exist.', inline=False)
+    embed.add_field(name='/help', value='See this menu.', inline=False)
+    await interaction.response.send_message(embed=embed)
 
 # If command is not found, it will be handled by this event
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send(f"Command not found, {ctx.author.mention}")
+        pass
     else:
         raise error
 
---
+++ something.py
@@ -0,0 +1,14 @@
+# Develop a discord slash command
+#
+# This example uses the discord.py rewrite branch.
+# The rewrite branch is currently in beta, and is not guaranteed to be stable.
+# The rewrite branch can be installed with pip:
+# `pip install -U git+
+#
+# This example requires the 'members' privileged intent to use the Member converter.
+# Go to https://discord.com/developers/applications to create a new application.
+# Click on the "Bot" tab, add a bot and copy the token.
+# Repl.it is sensitive to spaces. Make sure there are no trailing spaces in the token.
+# Click on the "OAuth2" tab.
+# Under "scopes" tick the "bot" checkbox.
+# Under "Bot Permissions" tick the checkboxes for "Send Messages" and "Embed Links".
